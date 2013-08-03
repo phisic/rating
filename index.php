529d7fba5741bc1b65a023bbf98d0ab6d7177447
Makhmud <mahmudbekstudio@mail.ru>
2013-08-03 14:19:09 +0500

    test

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-ini_set('display_errors', 1);
+ini_set('display_errors', 0);
 // change the following paths if necessary
 $yii='Yii/yii.php';
 $config=dirname(__FILE__).'/protected/config/main.php';
@@ -10,5 +10,7 @@ $config=dirname(__FILE__).'/protected/config/main.php';
 //defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
 
 require_once($yii);
+
 require_once('protected/components/WebApplication.php');
 Yii::createApplication('WebApplication', $config)->run();
+//test
