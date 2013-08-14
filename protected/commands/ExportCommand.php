96dbd756eba623e27ddda3190951ef44668bee3c
phisic <murod9@gmail.com>
2013-08-14 12:02:08 +0500

    export bio

--- protected/commands/ExportCommand.php
+++ protected/commands/ExportCommand.php
@@ -7,20 +7,7 @@ class ExportCommand extends CConsoleCommand {
     public function actionBillionaires() {
         $list = json_decode(file_get_contents('../data/billionaires.json'));
         $ratingName = 'Most Famous Billionaires in the World';
-        $date = date('Y-m-d H:i:s');
-        $c = new CDbCriteria(array('select' => 'Id'));
-        $c->addColumnCondition(array('Name' => $ratingName));
-        $row = Yii::app()->db->getCommandBuilder()->createFindCommand('rating', $c)->queryRow();
-        if (empty($row)) {
-            Yii::app()->db->getCommandBuilder()->createInsertCommand('rating', array(
-                'Name' => $ratingName,
-                'CategoryId' => 1,
-                'DateCreated' => $date
-            ))->execute();
-            $ratingId = Yii::app()->db->getCommandBuilder()->getLastInsertID('rating');
-        } else {
-            $ratingId = $row['Id'];
-        }
+        $ratingId = $this->createRating($ratingName);
 
         foreach ($list as $i) {
             $url = $this->forbesUrl . $i[2];
@@ -57,38 +44,12 @@ class ExportCommand extends CConsoleCommand {
         $newItems = 0;
         foreach ($r['data']['chartList'] as $c) {
             $name = 'Most Popular ' . $c['visibleName'];
-            $c1 = new CDbCriteria(array('select' => 'Id'));
-            $c1->addColumnCondition(array('Name' => $name));
-
-            $rexist = Yii::app()->db->getCommandBuilder()->createFindCommand('rating', $c1)->queryRow();
-            if (empty($rexist)) {
-                Yii::app()->db->getCommandBuilder()->createInsertCommand('rating', array(
-                    'Name' => $name,
-                    'CategoryId' => 1,
-                    'DateCreated' => $date
-                ))->execute();
-                $rId = Yii::app()->db->getCommandBuilder()->getLastInsertID('rating');
-            }
-            else
-                $rId = $rexist['Id'];
+            $rId = $this->createRating($name);
 
             foreach ($c['entityList'] as $i) {
-                $c2 = new CDbCriteria(array('select' => 'Id'));
-                $c2->addColumnCondition(array('Keyword' => $i['title']));
-                $exist = Yii::app()->db->getCommandBuilder()->createFindCommand('item', $c2)->queryRow();
-                echo $i['title'] . "\n";
-                if (empty($exist)) {
-                    $data = array(
-                        'Keyword' => $i['title'],
-                        'Description' => '',
-                        'Image' => isset($i['imageInfo']['url']) ? $i['imageInfo']['url'] : null,
-                        'DateCreated' => $date,
-                    );
-                    Yii::app()->db->getCommandBuilder()->createInsertCommand('item', $data)->execute();
-                    $iId = Yii::app()->db->getCommandBuilder()->getLastInsertID('item');
-                    Yii::app()->db->getCommandBuilder()->createInsertCommand('rating2item', array('ItemId' => $iId, 'RatingId' => $rId))->execute();
-                    $newItems++;
-                }
+                $iId = $this->createItem($i['title'], isset($i['imageInfo']['url']) ? $i['imageInfo']['url'] : null);
+                Yii::app()->db->getCommandBuilder()->createInsertCommand('rating2item', array('ItemId' => $iId, 'RatingId' => $rId))->execute();
+                $newItems++;
             }
         }
         echo 'inserts=' . $newItems . "\n";
@@ -138,6 +99,7 @@ class ExportCommand extends CConsoleCommand {
     }
 
     public function actionBio() {
+        $date = date('Y-m-d H:i:s');
         $p = new StringParser();
         $pmenu = $p->url('http://www.biography.com/people')->between('<span>Best Known For</span>', '<li class="has-submenu mainItem">');
         $u = array();
@@ -155,34 +117,73 @@ class ExportCommand extends CConsoleCommand {
             $t = $a->between('>', '<')->get();
             $u[$h] = $t;
         }
-        foreach ($u as $url => $name) {
+        foreach ($u as $url => $categoryName) {
+            $rId = $this->createRating('Most Famous ' . $categoryName);
             $page = 1;
             $url = 'http://www.biography.com' . $url . '/all?view=gallery&sort=last-name&page=';
-            echo $name . "\n";
+            echo $categoryName . "\n";
             $fetch = true;
             while (true) {
                 $pc = $p->url($url . $page)->between('<ul class = "gallery-list">', '<!-- Center: End -->');
                 $pager = $p->between('<li class="current">', '</li>');
-                
+
                 $items = array();
-                
+
                 while ($p2 = $pc->between('<h3 class="dot">', '</h3>')) {
                     $items[] = $p2->between('"', '"')->get();
                 }
 
                 foreach ($items as $iUrl) {
-                    $iUrl = 'http://www.biography.com'.$iUrl;
+                    $iUrl = 'http://www.biography.com' . $iUrl;
                     $name = $p->url($iUrl)->between('<span class="dot"  id="name-of-profile-title" >', '</span>')->get();
                     $img = $p->between('<div id="profile-photo-container">', '</div>')->between('"', '"')->get();
-                    $cont = $p->cut('<div id="profile-biography">')->removeTags(array('div','h3'))->between('<p>','</p>')->get();
+                    $cont = $p->cut('<div id="profile-biography">')->removeTags(array('div', 'h3'))->between('<p>', '</p>')->get();
+
                     Yii::app()->db->getCommandBuilder()->createInsertCommand();
                 }
-                
+
                 if (empty($pager) || $pager->get() != $page)
                     break;
                 $page++;
             }
         }
+    }
+
+    protected function createRating($name) {
+        $date = date('Y-m-d H:i:s');
+        $c1 = new CDbCriteria(array('select' => 'Id'));
+        $c1->addColumnCondition(array('Name' => $name));
+
+        $rexist = Yii::app()->db->getCommandBuilder()->createFindCommand('rating', $c1)->queryRow();
+        if (empty($rexist)) {
+            Yii::app()->db->getCommandBuilder()->createInsertCommand('rating', array(
+                'Name' => $name,
+                'CategoryId' => 1,
+                'DateCreated' => $date
+            ))->execute();
+            return Yii::app()->db->getCommandBuilder()->getLastInsertID('rating');
+        }
+        else
+            return $rexist['Id'];
+    }
+
+    protected function createItem($keyword, $image) {
+        $date = date('Y-m-d H:i:s');
+        $c2 = new CDbCriteria(array('select' => 'Id'));
+        $c2->addColumnCondition(array('Keyword' => $keyword));
+        $exist = Yii::app()->db->getCommandBuilder()->createFindCommand('item', $c2)->queryRow();
+        if (empty($exist)) {
+            $data = array(
+                'Keyword' => $keyword,
+                'Description' => '',
+                'Image' => $image,
+                'DateCreated' => $date,
+            );
+            Yii::app()->db->getCommandBuilder()->createInsertCommand('item', $data)->execute();
+            return Yii::app()->db->getCommandBuilder()->getLastInsertID('item');
+        }
+        else
+            return $exist['Id'];
     }
 
 }
