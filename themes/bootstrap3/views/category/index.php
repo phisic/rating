20deba0a0f835d6cfc1f54180e9cb973117e55d7
MUROD <murod@TOSHIBA-PC.(none)>
2013-08-23 20:39:39 +0500

    cat

--- themes/bootstrap3/views/category/index.php
+++ themes/bootstrap3/views/category/index.php
@@ -1,24 +1,21 @@
 <div class="row rating">
 
     <?php
-    $cnt = ceil(count($ratings) / 5);
+    $cnt = ceil(count($ratings) / 4);
     foreach (array_chunk($ratings, $cnt) as $r3) {
         ?>
-        <div class="col-sm-6 col-md-2">
-            
-                <?php foreach ($r3 as $r) { ?> 
-                        <div class="thumbnail">
-                            <div><strong><?= $r['Name'] ?></strong></div>
-                            <img src="/rt/r<?= $r['Id'] ?>-200x200.jpg">
-                            <?php  foreach ($r['i'][$r['Id']] as $n=>$i){?>
-                            <div><b>#<?=$n+1?>.</b> <?=$i['Keyword']?></div>
-                            <?php }?>
-                            <div><a href="">View full rating</a> 1267 items</div>
-                            <br>
-                            <div><label class="label-success label">Growing</label> <a href="">1267 items</a></div>
-                            <div><label class="label-important label">Losing</label> <a href="">1267 items</a></div>
-                        </div>
-<?php } ?>
+        <div class="col-sm-6 col-md-3">
+
+            <?php foreach ($r3 as $r) { ?> 
+                <div class="thumbnail">
+                    <a href="" title="View full rating <?= $r['Name'] ?>">
+                    <div><strong><?= $r['Name'] ?></strong></div>
+                    <div><img alt="<?= $r['Name'] ?>" src="/rt/r<?= $r['Id'] ?>-200x200.jpg"></a></div>
+                    <?php foreach ($r['i'][$r['Id']] as $n => $i) { ?>
+                        <div><b>#<?= $n + 1 ?>.</b> <?= $i['Keyword'] ?></div>
+                    <?php } ?>
+                </div>
+            <?php } ?>
         </div>
     <?php } ?>
 </div>
