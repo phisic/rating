d3ba4308c7904bfb01a26150616290b09f5d8dbc
phisic <murod9@gmail.com>
2013-08-29 11:20:10 +0500

    rank

--- protected/commands/RatingCommand.php
+++ protected/commands/RatingCommand.php
@@ -9,35 +9,46 @@ class RatingCommand extends CConsoleCommand {
         $c->order = 'RankDate';
         $c->limit = 100;
         $px = new StringParser();
+        $client = 0;
         do {
             $r = Yii::app()->db->getCommandBuilder()->createFindCommand('rating', $c)->queryAll();
             foreach ($r as $row) {
                 if ($pos = strpos($row['Keyword'], ','))
                     $row['Keyword'] = substr($row['Keyword'], 0, $pos);
                 if ($pos = strpos($row['Keyword'], ' '))
-                    $row['Keyword'] = '"'.$row['Keyword'].'"';
-                $row['Keyword'] = $row['Context'] . ' '. $row['Keyword'];
+                    $row['Keyword'] = '"' . $row['Keyword'] . '"';
+                $row['Keyword'] = $row['Context'] . ' ' . $row['Keyword'];
+                $keyword = urlencode($row['Keyword']);
+
+                $url[0] = 'https://www.google.com/search?client=ubuntu&channel=fs&q=' . $keyword . '&ie=utf-8&oe=utf-8';
+                $url[1] = 'https://www.google.com/#fp=fef978c8887d2f8f&psj=1&q=' . $keyword;
+                $url[2] = 'https://www.google.com/search?sclient=psy-ab&q=' . $keyword . '&oq=' . $keyword . '&gs_l=hp.3...46404.46404.12.65758.1.1.0.0.0.0.188.188.0j1.1.0....0...1c.1.25.psy-ab..19.4.791.DMko7MTGOMk&pbx=1&bav=on.2,or.r_qf.&bvm=bv.51156542%2Cd.bGE%2Cpv.xjs.s.en_US.iz6Z5q8RWbs.O&fp=34b4cd05192a2aa8&biw=1366&bih=342&tch=1&ech=1&psi=q2AeUpezLIqJ4ATNq4FQ.1377722542206.15';
+                $client = rand(0,1);
+                echo $row['Keyword'] . ' C='.$client."\n";
                 
-                $url = 'https://www.google.com/search?client=ubuntu&channel=fs&q='. urlencode($row['Keyword']).'&ie=utf-8&oe=utf-8';
-                $out = $px->proxy(urlencode($row['Keyword']));
-                sleep(3);
-                echo $row['Keyword'] . "\n";
-                $p = new StringParser($out);
-                $rank = $p->between('<div id="resultStats">About ', ' results');
+                $p = new StringParser($px->grab($url[2], $client));
+                $p = $p->cut('About Google');
+                if (empty($p)) {
+                    sleep(10);
+                    continue;
+                }
+                $p->offset+=15;
+                $rank = $p->between('About ', ' results');
+                sleep(rand(0, 3));
                 if (!$rank)
                     continue;
                 $rank = $rank->remove(',')->get();
+                $rank = ceil($rank/1000);
                 echo $rank . "\n";
                 $c2 = new CDbCriteria();
-                $c2->addColumnCondition(array('ItemId' => $row['ItemId'], 'RatingId'=>$row['RatingId']));
-                
+                $c2->addColumnCondition(array('ItemId' => $row['ItemId'], 'RatingId' => $row['RatingId']));
+
                 $delta = 0;
-                if(!is_null($row['Rank']))
+                if (!is_null($row['Rank']))
                     $delta = $rank - $row['Rank'];
-               
+
                 $date = date('Y-m-d H:i:s');
-                $r = Yii::app()->db->getCommandBuilder()->createInsertCommand('item_history', array('ItemId' => $row['ItemId'],'RatingId'=>$row['RatingId'], 'Rank' => $rank, 'DateCreated' => $date))->execute();
-                $r = Yii::app()->db->getCommandBuilder()->createUpdateCommand('rating2item', array('Rank' => $rank, 'RankDate' => $date, 'RankDelta'=>$delta), $c2)->execute();
+                $r = Yii::app()->db->getCommandBuilder()->createUpdateCommand('rating2item', array('Rank' => $rank, 'RankDate' => $date, 'RankDelta' => $delta), $c2)->execute();
             }
         } while ($r);
     }
@@ -56,3 +67,7 @@ class RatingCommand extends CConsoleCommand {
     }
 
 }
+
+//https://www.google.co.uz/search?sclient=psy-ab&q=Teachers+%22Jean+Harris%22&oq=Teachers+%22Jean+Harris%22&gs_l=hp.3...46404.46404.12.65758.1.1.0.0.0.0.188.188.0j1.1.0....0...1c.1.25.psy-ab..19.4.791.DMko7MTGOMk&pbx=1&bav=on.2,or.r_qf.&bvm=bv.51156542%2Cd.bGE%2Cpv.xjs.s.en_US.iz6Z5q8RWbs.O&fp=34b4cd05192a2aa8&biw=1366&bih=342&tch=1&ech=1&psi=q2AeUpezLIqJ4ATNq4FQ.1377722542206.15
+//https://www.google.co.uz/s?gs_rn=25&gs_ri=psy-ab&pq=teachers%20%22%20jean%20harris%22&cp=10&gs_id=6x1&xhr=t&q=Teachers%20%22Jean%20Harris%22&es_nrs=true&pf=p&sclient=psy-ab&oq=&gs_l=&pbx=1&bav=on.2,or.r_qf.&bvm=bv.51156542,d.bGE&fp=34b4cd05192a2aa8&biw=1366&bih=342&bs=1&tch=1&ech=2&psi=q2AeUpezLIqJ4ATNq4FQ.1377722542206.13
+//https://www.google.co.uz/search?sclient=psy-ab&q=Teachers+%22Jean+Harris%22+and&oq=Teachers+%22Jean+Harris%22+and&gs_l=hp.3..33i29i30l4.106386.107684.13.111377.4.3.1.0.0.0.288.685.0j1j2.3.0....0...1c.1.25.psy-ab..20.7.1465.47ky3tHrIGE&pbx=1&bav=on.2,or.r_qf.&bvm=bv.51156542%2Cd.bGE%2Cpv.xjs.s.en_US.iz6Z5q8RWbs.O&fp=34b4cd05192a2aa8&biw=1366&bih=342&tch=1&ech=1&psi=q2AeUpezLIqJ4ATNq4FQ.1377722542206.17
--- protected/components/StringParser.php
+++ protected/components/StringParser.php
@@ -7,22 +7,35 @@ class StringParser {
     protected static $proxy = 0;
     protected static $client = array(
         array(
-            'Connection: keep-alive',
-            'Cache-Control: max-age=0',
-            'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-            'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.17 (KHTML, like Gecko) Ubuntu Chromium/24.0.1312.56 Chrome/24.0.1312.56 Safari/537.17',
-            'Accept-Encoding: deflate,sdch',
-            'Accept-Language: en-US,en;q=0.8',
+            'Host: www.google.com',
+            'Accept: */*',
             'Accept-Charset: ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+            'Accept-Encoding: gzip,deflate',
+            'Accept-Language: en-US,en;q=0.8',
+            'Cookie: PREF=ID=c4c4285d83f832c6:U=6b262550f9a229a9:LD=en:TM=1362211581:LM=1372870960:GM=1:S=5OrGGZoI240JxD81; GDSESS=ID=654c1603fd6559fc:TM=1377751668:C=c:IP=89.146.104.222-:S=APGng0vWlPd3y-yKhu6ECr__DzsVlGN65w; NID=67=N9OYt-VNFigTFEyfX1OK3LIsOv423FxWSzmGWgMidsMXAK1cbyg3fR5x73LV1DNDnKdseL-iYEj3wb8oJIiMEaKV4TtX_kI5X7CYKIDkLhxIIGHElFJlypABDiFYul9LH6zXbxa9bQOpIfgTbGQvy5P4DJ2OGqwFRGbBdaY-ZSJeWbI7DyynS3Y; HSID=A8NAgJAqw9agGdNgq; APISID=P7iJ5FUlsWSzCn-j/Ais20q3wjKf8zOQZp; SID=DQAAANsAAACQDqoEc77cEmB2RM-prpC-Ahle5iWiANIYa33ilxFRESyIET9scvvH5bed6EIfhetHC4XlLL40DI21NHliafVvn6kSdcIOZbeUJ69xSX8P2zeahKTaSYieD6Or7Ac4WCSJWHyvtGiQ91QyaR4q7YkoXfKGBl9MZt2Qcy11GBto6irsehN8cdR2sCTp5lu8FdXNJEiXxxAIcroaihxjco8AuhaiTe8PvXSfJbmqUore2McG-H09n9K6ncPV8hjlSOVgwORRbQ6vf7lfMJe3PctC9zhu62jp8Igo2GTaR_iUrQ',
+            'Referer: http://www.google.com/',
+            'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.17 (KHTML, like Gecko) Ubuntu Chromium/24.0.1312.56 Chrome/24.0.1312.56 Safari/537.17',
+            'X-Chrome-Variations: CMK1yQEIl7bJAQiYtskBCKS2yQEIp7bJAQi7tskBCNyDygE=',
+        ),
+        array(
+            'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+            'Accept-Encoding: gzip, deflate',
+            'Accept-Language: en-US,en;q=0.5',
+            'Connection: keep-alive',
+            'Cookie: PREF=ID=2223fc803701a9c6:U=24c29306041da552:FF=0:LD=en:TM=1377712906:LM=1377756813:S=-bg4rPBcsIYnqFwb; NID=67=TAptg1VW9zkoIqenMaigDxN7RAl2NWdgQFPUHxqBS_3-dJIFtLkl_DOlphX8ikg4vbhOlKTMc4F86i52JQi2SmLMYJ9gZbvYblIkH2u0vQR7e6V03aC5tOAQlZZgjg8SH1pyrPrRsZf-FmlmNok8BEDyKeVAub_lO6-RSd1upyUWQCZAqBor6Q; GDSESS=ID=3e7edde6fc2c5d97:TM=1377756364:C=c:IP=89.146.104.222-:S=APGng0uNd8HFNSzn3T7izFbTg0g45grD-Q; S=account-recovery=P6iTi2aVDV8; SID=DQAAAKEAAADDlI_nT6Pn8XSKSxcYS_D7OjDQNdho8TPcm4CXi9B7mA9MZBoNORMpfqYyxmKTFwZBL-NcVvXGhTzT-O8ko6gEd3p64c-zG2A5Z7MGvk6fkHj5sDET9sgdMWWfYMHt32aiSApdok9n_Zmt62svHD58PwFAiIN32iYzqgU1p119g8ETVZea2FTBkc_0hYq-es71jfKrDrhe77a8vhY7oJWBs89TJW1piAdojr4zxYKf2A; HSID=ABgQ7jla8pdG-otZ3; SSID=A3gfWDyewNsL2qrua; APISID=52PlNameB0WwQqeZ/AbQBA0dtpwjRiPsep; SAPISID=xv4LTnf32FqsyHCo/AThi9jieKSyVSG3is',
+            'Host: www.google.com',
+            'Referer: https://www.google.com/search?biw=1366&bih=342&noj=1&sclient=psy-ab&q=PHP6&oq=PHP6&gs_l=serp.3...0.0.1.9088.0.0.0.0.0.0.0.0..0.0....0...1c..26.serp..0.0.0.cHxCzMBwQ7M',
+            'User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:19.0) Gecko/20100101 Firefox/19.0',
         ),
         array(
             'User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:19.0) Gecko/20100101 Firefox/19.0',
             'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
             'Accept-Language: en-US,en;q=0.5',
             'Accept-Encoding: deflate',
-            'Connection: keep-alive',
-            'Pragma: no-cache',
-            'Cache-Control: no-cache',
+            'Host: www.google.com',
+            //'Pragma: no-cache',
+//'Cache-Control: no-cache',
+            'Referer: https://www.google.com/'
         ),
     );
 
@@ -83,6 +96,29 @@ class StringParser {
         return $this;
     }
 
+    public function compatible_gzinflate($gzData) {
+
+        if (substr($gzData, 0, 3) == "\x1f\x8b\x08") {
+            $i = 10;
+            $flg = ord(substr($gzData, 3, 1));
+            if ($flg > 0) {
+                if ($flg & 4) {
+                    list($xlen) = unpack('v', substr($gzData, $i, 2));
+                    $i = $i + 2 + $xlen;
+                }
+                if ($flg & 8)
+                    $i = strpos($gzData, "\0", $i) + 1;
+                if ($flg & 16)
+                    $i = strpos($gzData, "\0", $i) + 1;
+                if ($flg & 2)
+                    $i = $i + 2;
+            }
+            return @gzinflate(substr($gzData, $i, -8));
+        } else {
+            return false;
+        }
+    }
+
     protected function getContent($url) {
         $h = md5($url);
         $c = new CDbCriteria(array('select' => 'Content'));
@@ -95,21 +131,18 @@ class StringParser {
         return $cont;
     }
 
-    public function grab($url) {
+    public function grab($url, $client = 0) {
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-            'User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:20.0) Gecko/20100101 Firefox/20.0',
-            'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-            'Accept-Language: en-US,en;q=0.5',
-            'Accept-Encoding: deflate',
-            'Accept-Charset: ISO-8859-1,utf-8;q=0.7,*;q=0.7',
-            'Connection: Close'
-        ));
+        curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_0);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, self::$client[$client]);
+// $cook = 'PREF=ID=2223fc803701a9c6:U=24c29306041da552:FF=0:TM=1377712906:LM=1377719573:S=wZCm8AdfruxYZcOJ; NID=67=MSofsRpvzu38-1-WOMxZKT2ukoUceE21SLdzNhuKd1bTrxFG-SvO413Zx-7OD0InhLNGCkmKMDWXPDRL8ATM8o6JqJzGidvpGwhTgOIlAwboKxbDmrRNya6fSEaQj-xl; GDSESS=ID=2f3327048613e1ee:TM=1377719528:C=c:IP=89.146.104.222-:S=APGng0vARGksmDDSsiN9IA_ea6YrEPjF5w';
+//curl_setopt($ch, CURLOPT_COOKIE, $cook);
+
 
         $response = curl_exec($ch);
+        $response = $this->compatible_gzinflate($response);
         file_put_contents('1.txt', $response);
         curl_close($ch);
         return $response;
@@ -123,15 +156,15 @@ class StringParser {
         }
         $proxyName = $k[self::$proxy];
         self::$proxy++;
-        
+
         $url = str_replace('{$q}', $q, $proxy[$proxyName]['url']);
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
-        $client = rand(0, count(self::$client)-1);
+        $client = rand(0, count(self::$client) - 1);
         curl_setopt($ch, CURLOPT_HTTPHEADER, self::$client[$client]);
-        echo 'Proxy=' . $proxyName .' Client='.$client. "\n";
+        echo 'Proxy=' . $proxyName . ' Client=' . $client . "\n";
 
         curl_setopt($ch, CURLOPT_COOKIE, $proxy[$proxyName]['cook']);
 
