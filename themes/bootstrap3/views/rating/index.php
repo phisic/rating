63d01f2feee6f437300229b3d6c68e0cec0e43f4
MUROD <murod@TOSHIBA-PC.(none)>
2013-09-03 21:50:43 +0500

    table

--- themes/bootstrap3/views/rating/index.php
+++ themes/bootstrap3/views/rating/index.php
@@ -1,24 +1,23 @@
 <h1><?=$rating['Name']?></h1>
-<div class="list_item clearfix">
-    <div class="col-lg-1 padding_left0"><strong>#</strong></div>
-    <div class="col-lg-2 padding_left0"><strong>Image</strong></div>
-    <div class="col-lg-4 padding_left0"><strong>Name</strong></div>
-    <div class="col-lg-2 padding_left0" style='text-align: center;'><strong>Web score</strong></div>
-    <div class="col-lg-2 padding_left0"><strong>+/-</strong></div>
-    <div class="col-lg-1 padding_left0"><strong>Updated</strong></div>
-</div>
+<table class="table table-condensed">
+    <tr>
+    <th>#</th>
+    <th>Image</th>
+    <th>Name</th>
+    <th>Web score</th>
+    <th>Updated</th>
+</tr>
     <?php foreach ($items as $n=>$i) {?>
-    <div class="list_item clearfix">
-            <div class="col-lg-1 padding_left0">
+   <tr>
+            <td>
                 <?=($n+1+$pager->offset)?>
-            </div>
-            <div class="col-lg-2 padding_left0"><img class="img-thumbnail" src="/rt/p<?=$i['Id'].'-200x200.jpg'?>"></div>
-            <div class="col-lg-4 padding_left0">
+            </td>
+            <td><img class="img-thumbnail" src="/rt/p<?=$i['Id'].'-200x200.jpg'?>"></td>
+            <td>
                     <strong><?=$i['Keyword']?></strong>
-                    <div><?=isset($i['Description'])?$i['Description']:''?></div>
-            </div>
-            <div class="col-lg-2 padding_left0" style="text-align: center;"><strong><?=$i['Rank']?></strong></div>
-            <div class="col-lg-2 padding_left0">
+                    <div><?=isset($i['Description'])?$i['Description']:''?></td>
+            </td>
+            <td><strong><?=$i['Rank']?></strong>
                 <?php if($i['RankDelta']==0){}?>
                 
                 <?php if($i['RankDelta']>0){?>
@@ -26,9 +25,10 @@
                 <?php }elseif($i['RankDelta']<0){ ?>
                 <span class="label label-danger"><?=$i['RankDelta']?></span>
                 <?php }?>
-            </div>
-            <div class="col-lg-1 padding_left0"><?=$i['RankDate']?></div>
-        </div> 
+            </td>
+            <td><?=date('j/m/y',strtotime($i['RankDate']))?></td>
+       </tr>
        
     <?php } ?>
+    </table>
 <?php $this->widget('CLinkPager', array('htmlOptions' => array('class' => 'pager'), 'pages' => $pager)); ?>
