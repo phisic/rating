860a8cb99dd814677eb6b095bbb41ffae0814489
phisic <murod9@gmail.com>
2013-10-20 20:59:22 +0500

    dele

--- themes/bootstrap3/jsOLD/scripts.js
+++
@@ -1,9 +0,0 @@
-$(document).on('mouseover', '.category_list a', function(){
-	$(this).parent().parent().find('a.active').removeClass('active');
-	$(this).addClass('active');
-	$($(this).attr('data-target')).parent().find('div').hide();
-	$($(this).attr('data-target')).show();
-});
-$(document).on('mouseout', '.category_list a', function(){
-	//$(this).removeClass('active');
-});
